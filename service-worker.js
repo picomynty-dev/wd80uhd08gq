2d03271f85134837a13c17782206c50a3d8f63c4
picomynty-dev <picomynty@gmail.com>
2026-07-27 12:53:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-fit-plan-v321-20260726-1';
+const CACHE_NAME = 'my-fit-plan-v322-20260726-1';
 const APP_SHELL = [
   './',
   './index.html',
@@ -14,7 +14,6 @@ const APP_SHELL = [
   './js/exercises.js',
   './js/exercises-extra.js',
   './js/premium-data.js',
-  './js/media-bundle.js',
   './js/search.js',
   './js/visuals.js',
   './js/plans.js',
